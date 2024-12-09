84bf6d6d8b0d6fafe24321babbe284a2eef881e0
Irrfann29 <mrirfanhashmi2303@gmail.com>
2024-12-09 13:07:30 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-let rep = "";
-let ranNum = Math.floor(Math.random() * 10)
-for (let i = 1 ; i<= 120 ; i++){
-    ranNum = Math.floor(Math.random() * 10)
-    rep += `<h3 id="numbers">${ranNum}</h3>`
+function ranVal() {
+    let rep = "";
+    let ranNum = Math.floor(Math.random() * 10)
+    for (let i = 1; i <= 120; i++) {
+        ranNum = Math.floor(Math.random() * 10)
+        rep += `<h3 id="numbers">${ranNum}</h3>`
+    }
+    document.querySelector("#mainSection").innerHTML = rep;
 }
-document.querySelector("#mainSection").innerHTML = rep ;
+function timer() {
+    let timer = document.querySelector("#timer");
+    let runTime = 11;
+    var timeUp = setInterval(function () {
+
+        if (runTime > 0) {
+            runTime--;
+            timer.textContent = runTime;
+            
+        }
+        else{
+            clearInterval(timeUp)
+        }
+
+    }, 1000);
+}
+function hitBtn(){
+    let randomVal = Math.floor(Math.random()*10);
+    document.querySelector("#hitBox").textContent = randomVal;
+
+}
+
+
+
+hitBtn()
+ranVal()
+timer()
