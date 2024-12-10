26e73717f7943ef74218de7d84f5348944a50f0a
Irrfann29 <mrirfanhashmi2303@gmail.com>
2024-12-10 08:30:32 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var randomVal = 0;
 var score = 0;
 var runTime = 6;
 var timeUp;
+
 function ranVal() {
     let rep = "";
     let ranNum = Math.floor(Math.random() * 10)
@@ -15,50 +16,34 @@ function ranVal() {
 function timer() {
     var runTime = 60;
     timeUp = setInterval(function () {
-
         if (runTime > 0) {
             runTime--;
-            document.querySelector("#timer").textContent = runTime;
-            
+            document.querySelector("#timer").textContent = runTime;   
         }
         else{
             clearInterval(timeUp);
             document.querySelector("#mainSection").innerHTML = `<h1>Game Over </h2>`
             document.querySelector("#mainSection").style.fontColor = "black"
         }
-
     }, 1000);
 }
 function hitBtn(){
     randomVal = Math.floor(Math.random()*10);
     document.querySelector("#hitBox").textContent = randomVal;
-
 }
 function incScore (){
     score+=5;
     document.querySelector("#incScore").textContent = score;
-
 }
 
 document.querySelector("#mainSection").addEventListener("click",function(detail){
     let match = Number(detail.target.innerHTML);
     if (randomVal === match){
-        
         incScore();
         ranVal();
-        hitBtn()
-        
-        
-    }
-    
-    
-    
+        hitBtn()   
+    }   
 })
-function updScore(){
-
-}
-
 timer()
-
 hitBtn()
 ranVal()
